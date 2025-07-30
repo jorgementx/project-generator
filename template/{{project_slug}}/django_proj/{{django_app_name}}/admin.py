ab5aa1f02de5c63714eee78392f316fe93ca7d39
Jorge Mentxaka <jmentxaka@purpleblob.net>
2025-07-30 15:54:08 +0200

    feat(backend): proper configuration for using username instead of email
    extra: launch.json and tasks.json added to run project easily using
    vscode debugger

--- template/{{project_slug}}/django_proj/{{django_app_name}}/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from unfold.admin import ModelAdmin
-from .models import *
-
-
-@admin.register(User)
-class UserAdmin(ModelAdmin):
-    exclude = ['password', 'username']
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
